a01cc88b76edef8f1362ef364ba7f374e82e9589
DamianIvancic <47188413+DamianIvancic@users.noreply.github.com>
2019-10-15 16:56:43 +0200

    Interaction: place the asterisk in the correct place

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var paragraph = document.getElementById('demo');
+var letters = document.getElementsByClassName('letter');
+var apostropheMap = new Map();
+
+for(i=0; i<letters.length; i++)
+{
+    letters[i].onclick = myFunction;
+    apostropheMap.set(letters[i], false);
+}
+
+function myFunction(event){
+
+    if(apostropheMap.get(event.srcElement) == false){
+        
+        for(i=0; i<letters.length; i++){
+            
+            if(letters[i].innerHTML.includes("'"))
+               letters[i].innerHTML = letters[i].innerHTML.replace("'", "");
+        }
+
+        apostropheMap.forEach(resetApostropheValues);
+
+        event.srcElement.innerHTML += "'";
+        apostropheMap.set(event.srcElement, true)
+    }   
+}
+
+
+function resetApostropheValues(value, key, map){  
+    map.set(key, false);
+}
