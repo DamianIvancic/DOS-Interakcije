d26f21b49d879b10a712fc3a0b525e05e991a24d
exordiumgames-Damian <damian@exordiumgames.com>
2019-10-16 17:41:47 +0200

    Added loading from file & correctness verification

--- index.js
+++ index.js
@@ -1,32 +1,72 @@
-//Just a comment to see if push is working
-var paragraph = document.getElementById('demo');
-var letters = document.getElementsByClassName('letter');
-var apostropheMap = new Map();
 
-for(i=0; i<letters.length; i++)
-{
-    letters[i].onclick = myFunction;
-    apostropheMap.set(letters[i], false);
+
+let correctApostropheIdxs = new Map();
+let currentApostropheIdxs = new Map();
+
+let checkButton = document.getElementById('check');
+checkButton.onclick = verifyCorrectness;
+
+document.getElementById('title').innerHTML = '<img src="Images\\checkmark.jpg">';
+
+setup();
+
+function setup(){
+
+    let sentences = Object.keys(data);
+    let apostropheValues = Object.values(data);
+
+    for(i=0; i<sentences.length; i++){
+
+        let parent = document.createElement("p");
+        let characters = sentences[i].split("");
+
+        for(let char of characters){
+            let element = document.createElement("span");
+            element.className = "sentence"+ i;
+            element.innerHTML = char;
+            element.onclick = placeApostrophe;
+            parent.appendChild(element);
+        }
+    
+        correctApostropheIdxs.set("sentence"+i, apostropheValues[i]);
+        document.body.appendChild(parent);
+    } 
 }
 
-function myFunction(event){
+function placeApostrophe(event){
+   
+    let spans = document.getElementsByClassName(event.srcElement.className);
 
-    if(apostropheMap.get(event.srcElement) == false){
-        
-        for(i=0; i<letters.length; i++){
+    for(i=0; i<spans.length; i++){
+
+        if(spans[i].innerHTML.includes("'"))
+           spans[i].innerHTML = spans[i].innerHTML.replace("'", "");
+
+        if(spans[i] == event.srcElement)
+            currentApostropheIdxs.set(event.srcElement.className, i+1);
             
-            if(letters[i].innerHTML.includes("'"))
-               letters[i].innerHTML = letters[i].innerHTML.replace("'", "");
+    }
+
+    event.srcElement.innerHTML += "'";
+}
+
+function verifyCorrectness(){
+
+    let result = document.getElementById('result');
+    
+    if(currentApostropheIdxs.size == correctApostropheIdxs.size){
+
+        for(let key of currentApostropheIdxs.keys()){
+
+            if(currentApostropheIdxs.get(key) != correctApostropheIdxs.get(key)){
+                result.innerHTML = "Wrong";
+                return;
+            }            
         }
 
-        apostropheMap.forEach(resetApostropheValues);
-
-        event.srcElement.innerHTML += "'";
-        apostropheMap.set(event.srcElement, true)
-    }   
-}
-
-
-function resetApostropheValues(value, key, map){  
-    map.set(key, false);
+        result.innerHTML = "Correct";
+    }
+    else{
+        result.innerHTML = "Wrong";
+    }
 }
